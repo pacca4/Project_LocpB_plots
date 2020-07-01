c57788f493a5ba9a1858450f56197d24ee1b91de
RoccoA97 <roccoardino@gmail.com>
2020-07-01 12:56:46 +0200

    Redefinition of Toy MC (under test)

--- macros/MC/ToyMC_test.cc
+++ macros/MC/ToyMC_test.cc
@@ -42,18 +42,140 @@ const double phi_Mu1_inf = 3.05;
 
 
 
-void test() {
+double testRunToyModel(Int_t nbins=100) {
+	// ************************************************************************** //
+	// True distribution
+	// ************************************************************************** //
 	RooRealVar MuMuG_invMass("MuMuG_invMass", "Reconstructed Mass", 0, 2, "[a.u.]]");
 	RooRealVar Lambda("LBkgComb", "combinatorial Bkg life, Left side", -1, "1/[a.u.]");
+	RooRealVar SigMean("SigMean","Signal mean", 1, "[a.u.]");
+	RooRealVar SigSD("SigSD","Signal s.d.", 0.1, "[a.u.]");
 
 	RooExponential ExpDecay("Exp Decay", "Exponential Decay", MuMuG_invMass, Lambda);
 
-	RooDataSet* data = ExpDecay.generate(MuMuG_invMass, 100000);
-	RooPlot* MuMuG_invMass_frame = MuMuG_invMass.frame();
-	data->plotOn(MuMuG_invMass_frame);
-	ExpDecay.plotOn(MuMuG_invMass_frame);
+	RooGaussian Signal("signal","signal PDF", MuMuG_invMass, SigMean, SigSD);
 
-	MuMuG_invMass_frame->Draw();
+
+	// --- Construct signal+background PDF ---
+	RooRealVar nsig("nsig","#signal events",200, 0., 20000);
+	RooRealVar nbkg("nbkg","#background events",2000, 0., 20000);
+	RooAddPdf model("model","g+a",RooArgList(Signal,ExpDecay), RooArgList(nsig,nbkg));
+
+	RooDataSet* data = model.generate(MuMuG_invMass, 10000);
+	TH1* h = data->createHistogram("h", MuMuG_invMass, nbins);
+
+
+	// RooPlot* MuMuG_invMass_frame = MuMuG_invMass.frame();
+	// data->plotOn(MuMuG_invMass_frame);
+	// model.plotOn(MuMuG_invMass_frame);
+	// model.plotOn(MuMuG_invMass_frame, Components(ExpDecay), LineStyle(ELineStyle::kDashed));
+	// model.plotOn(MuMuG_invMass_frame, Components(Signal),   LineStyle(ELineStyle::kDashed), LineColor(kRed));
+
+	// MuMuG_invMass_frame->Draw();
+
+	// cout << nsig.getVal() << endl;
+	// cout << nbkg.getVal() << endl;
+	// cout << h->GetEntries() << endl;
+	// ************************************************************************** //
+
+
+	// ************************************************************************** //
+	// Toy test
+	// ************************************************************************** //
+	// Normalizations:
+	//		(#bkg/(#sig+#bkg))*((#events*binWidth)/(1-e^{-2}))
+	//		(#sig/(#sig+#bkg))*((#events*binWidth)/(~1))
+	TF1* f_num = new TF1("f_fit",  "[0]*[1]*exp([2]*x) + [3]*[4]*gaus(5)", 0, 2);
+	TF1* f_den = new TF1("f_true", "[0]*[1]*exp([2]*x) + [3]*[4]*gaus(5)", 0, 2);
+
+	double N_sig = nsig.getVal();
+	double N_bkg = nbkg.getVal();
+	double N_tot = h->GetEntries();
+	double bin_w = h->GetBinWidth(1);
+
+	double norm_0 = (N_bkg/(N_bkg+N_sig)) * N_tot * bin_w;
+	double norm_1 = (N_sig/(N_bkg+N_sig)) * N_tot * bin_w;
+
+	f_num->FixParameter(0, norm_0);
+	f_num->FixParameter(3, 1.0);    // SM signal strength
+	f_num->FixParameter(4, norm_1);
+	f_num->SetParameter(2, -1.0);
+	f_num->SetParameter(6,  1.0);
+	f_num->SetParameter(7,  0.1);
+
+	f_den->FixParameter(0, norm_0);
+	f_den->FixParameter(4, norm_1);
+	f_den->SetParameter(2, -1.0);
+	f_den->SetParameter(3,  1.0); // SM signal strength
+	f_den->SetParameter(6,  1.0);
+	f_den->SetParameter(7,  0.1);
+
+	// h->Draw();
+	h->Fit(f_num, "LQ0");
+	h->Fit(f_den, "LQ0");
+	h->Fit(f_num, "LQ0");
+	h->Fit(f_den, "LQ0");
+	h->Fit(f_num, "LQ0");
+	h->Fit(f_den, "LQ0");
+
+	// n = numerator
+	// d = denominator
+	long x_o;
+	double x_e_n;
+	double x_e_d;
+	double nll   = 0.;
+	// double nll_n = 0.;
+	// double nll_d = 0.;
+	// double lfsa  = 0.; // log factorial stirling approximation
+
+	for (int i=0; i<nbins; ++i) {
+		x_o   = h->GetBinContent(i+1);
+		x_e_n = f_num->Eval(h->GetBinCenter(i+1));
+		x_e_d = f_den->Eval(h->GetBinCenter(i+1));
+
+		// lfsa  = 0.5*log(2*M_PI*x_o) + x_o*log(x_o/exp(1.0));
+
+		nll  += (- x_e_n + x_e_d) + x_o*log(x_e_n/x_e_d);
+
+		// nll += - x_e + x_o*log(x_e) - lfsa;
+	}
+
+	nll *= -2.0;
+
+
+	//cout << "Negative Log Likelihood: " << nll << endl;
+
+	delete f_num;
+	delete f_den;
+	delete h;
+
+	return nll;
+	// ************************************************************************** //
+}
+
+
+
+void testSampleNLL(Int_t nsamples=1000, Int_t nbins1=100, Int_t nbins2=100, int patience=10) {
+	vector<double> nlls(0);
+
+	for (int i=0; i<nsamples; ++i) {
+		nlls.push_back(testRunToyModel(nbins2));
+		if(i%patience==0){
+			std::cout << "Progress " << int(i*100.0/nsamples) << " %\r";
+    			std::cout.flush();
+		}
+	}
+
+	double xmin = *min_element(nlls.begin(), nlls.end());
+	double xmax = *max_element(nlls.begin(), nlls.end());
+
+	TH1D* h = new TH1D("h", "h", nbins1, xmin, xmax);
+
+	for (int i=0; i<nsamples; ++i) {
+		h->Fill(nlls[i]);
+	}
+
+	h->Draw();
 }
 
 
