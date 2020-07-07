bce462d1e7b6359f11c0ec3f40e6d83ff948414d
RoccoA97 <roccoardino@gmail.com>
2020-07-07 23:50:56 +0200

    ToyMC v03 (under development)

---
+++ macros/MC/v03/ToyMC_DrawPDF_v03.cc
@@ -0,0 +1,62 @@
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooGaussian.h"
+#include "RooExponential.h"
+#include "RooAddPdf.h"
+#include "TCanvas.h"
+#include "RooPlot.h"
+#include "TAxis.h"
+#include "TH1.h"
+#include "TF1.h"
+
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+
+
+
+using namespace RooFit ;
+
+
+
+
+
+// ************************************************************************** //
+// Draw theoretical distribution
+// ************************************************************************** //
+void drawSigBkgPDF(Int_t nbins=100) {
+	// ************************************************************************** //
+	// True distribution
+	// ************************************************************************** //
+	// RooRealVar    Reco_mass("Reco_mass", "m_{#mu#mu#gamma}",         70.0, 150.0,        "GeV");
+	// RooRealVar    BkgTau   ("BkgTau",    "Bkg Lifetime",              1.0,   0.0, 100.0, "GeV"); // 6.33972e+01
+	// RooRealVar    ResoMean ("ResoMean",  "Mass Resolution Mean",     50.0,  20.0, 150.0, "GeV"); // 7.52686e+01
+	// RooRealVar    ResoSigma("ResoSigma", "Mass Resolution Sigma",    10.0,   0.1, 100.0, "GeV"); // 1.04042e+01
+	RooRealVar    Reco_mass("Reco_mass", "m_{#mu#mu#gamma}",       70.0, 150.0,  "GeV");
+	RooRealVar    LBkgTau  ("LBkgTau",   "Left Bkg Lifetime",       6.33972e+01, "GeV"); // 6.33972e+01
+	RooRealVar    ResoMean ("ResoMean",  "Mass Resolution Mean",    7.52686e+01, "GeV"); // 7.52686e+01
+	RooRealVar    ResoSigma("ResoSigma", "Mass Resolution Sigma",   1.04042e+01, "GeV"); // 1.04042e+01
+	RooRealVar    SigMean  ("SigMean",   "Signal Mean",           125.0,         "GeV");
+	RooRealVar    SigSigma ("SigSigma",  "Signal Sigma",            1.5,         "GeV");
+	RooGaussModel ResoGauss("ResoGauss", "Mass Resolution Gaussian", Reco_mass, ResoMean, ResoSigma);
+	RooDecay      LBkgExpo ("LBkgExpo",  "Left Bkg Convolved Decay", Reco_mass, LBkgTau,  ResoGauss, RooDecay::SingleSided);
+	RooGaussian   Signal   ("SigGauss",  "Signal Gaussian",          Reco_mass, SigMean,  SigSigma);
+
+	// --- Construct signal+background PDF --- //
+	RooRealVar NSig("NSig",  "#Signal events",       10, 0., 20000);
+	RooRealVar NBkg("NBkg",  "#Background events", 2000, 0., 20000);
+	RooAddPdf Model("Model", "B+mu*S", RooArgList(Signal,LBkgExpo), RooArgList(NSig,NBkg));
+
+	RooDataSet* data = Model.generate(Reco_mass, 100000);
+
+	RooPlot* Reco_mass_frame = Reco_mass.frame();
+	data->plotOn(Reco_mass_frame);
+	Model.plotOn(Reco_mass_frame);
+	Model.plotOn(Reco_mass_frame, Components(LBkgExpo), LineStyle(ELineStyle::kDashed));
+	Model.plotOn(Reco_mass_frame, Components(Signal),   LineStyle(ELineStyle::kDashed), LineColor(kRed));
+
+	Reco_mass_frame->Draw();
+	// ************************************************************************** //
+}
+// ************************************************************************** //
