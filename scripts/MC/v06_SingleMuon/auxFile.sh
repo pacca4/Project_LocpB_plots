84579a5fb100c22e633390382f4d7ba962b7f4d2
mastrovalentz <alevalentz@gmail.com>
2020-08-01 23:21:57 +0200

    Run single muon MC

--- scripts/MC/v06_SingleMuon/auxFile.sh
+++ scripts/MC/v06_SingleMuon/auxFile.sh
@@ -1,3 +1,4 @@
 #BSUB -J ToyMC_BatchMu_IsoTrk_v06_SingleMuon
-cd ..
-root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_IsoTrk_v06.cc('10', '100', '1600', '2');'
+#BSUB -o treeAna.log
+cd /lustre/cmswork/aavalent/CMSSW_10_2_6/src/PhysicsTools/NanoAODTools/python/postprocessing/SingleMuon_analysis/Project_LocpB_plots/scripts/MC/v06_SingleMuon
+root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_IsoTrk_v06.cc('100000', '100', '1600', '500');'
--- scripts/MC/v06_SingleMuon/run_ToyMC_BatchMu_IsoTrk_v06.sh
+++ scripts/MC/v06_SingleMuon/run_ToyMC_BatchMu_IsoTrk_v06.sh
@@ -25,7 +25,8 @@ do
 echo "Submitting BatchMu $mu"
 /bin/cat <<EOM > $AUXFILE
 #BSUB -J ToyMC_BatchMu_IsoTrk_v06_SingleMuon
-cd ..
+#BSUB -o treeAna.log
+cd /lustre/cmswork/aavalent/CMSSW_10_2_6/src/PhysicsTools/NanoAODTools/python/postprocessing/SingleMuon_analysis/Project_LocpB_plots/scripts/MC/v06_SingleMuon
 root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_IsoTrk_v06.cc('$4', '$5', '$6', '$mu');'
 EOM
 bsub -q "local-cms-long" < $AUXFILE
