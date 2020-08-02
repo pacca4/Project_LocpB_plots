0a41cb106e5ae5877e72b443843a328850b34fcf
mastrovalentz <alevalentz@gmail.com>
2020-08-02 16:37:16 +0200

    MC single muon 40 bins

--- scripts/MC/v06_SingleMuon/auxFile.sh
+++ scripts/MC/v06_SingleMuon/auxFile.sh
@@ -1,4 +1,4 @@
 #BSUB -J ToyMC_BatchMu_IsoTrk_v06_SingleMuon
 #BSUB -o treeAna.log
 cd /lustre/cmswork/aavalent/CMSSW_10_2_6/src/PhysicsTools/NanoAODTools/python/postprocessing/SingleMuon_analysis/Project_LocpB_plots/scripts/MC/v06_SingleMuon
-root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_IsoTrk_v06.cc('100000', '60', '1600', '500');'
+root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_IsoTrk_v06.cc('250000', '40', '1600', '400');'
