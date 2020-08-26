d8511f706b07bb1ae2ee8e96ddbdd4031ccfa9d9
RoccoA97 <roccoardino@gmail.com>
2020-08-26 16:48:57 +0200

    SingleMuon 30 bins 1000 ev

--- scripts/MC/v06_SingleMuon/auxFile.sh
+++ scripts/MC/v06_SingleMuon/auxFile.sh
@@ -1,4 +1,4 @@
 #BSUB -J ToyMC_BatchMu_IsoTrk_v06_SingleMuon
 #BSUB -o treeAna.log
-cd /lustre/cmswork/aavalent/CMSSW_10_2_6/src/PhysicsTools/NanoAODTools/python/postprocessing/SingleMuon_analysis/Project_LocpB_plots/scripts/MC/v06_SingleMuon
-root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_IsoTrk_v06.cc('250000', '40', '1000', '400');'
+cd /lustre/cmswork/ardino/HToJPsiG_ToMuMuG/LOCP_modB_Project/scripts/MC/v06_SingleMuon
+root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_SingleMuon_v06.cc('100000', '30', '1000', '300');'
