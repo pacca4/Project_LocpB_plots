83f409f22159f4da28a8e85ebc6e7c68efbca83c
RoccoA97 <roccoardino@gmail.com>
2020-08-29 00:35:52 +0200

    SingleMuon 20 bins

--- scripts/MC/v06_SingleMuon/auxFile.sh
+++ scripts/MC/v06_SingleMuon/auxFile.sh
@@ -1,4 +1,4 @@
 #BSUB -J ToyMC_BatchMu_IsoTrk_v06_SingleMuon
 #BSUB -o treeAna.log
 cd /lustre/cmswork/ardino/HToJPsiG_ToMuMuG/LOCP_modB_Project/scripts/MC/v06_SingleMuon
-root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_SingleMuon_v06.cc('100000', '30', '1000', '300');'
+root -l -b -q '../../../macros/MC/v06_SingleMuon/ToyMC_BatchMu_SingleMuon_v06.cc('100000', '20', '1000', '400');'
